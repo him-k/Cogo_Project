f783a1599693c8f79b015cfae218913ff62ae951
mlv-dheeraj <b20210@students.iitmandi.ac.in>
2024-07-10 14:54:04 +0530

    Backend updated

--- backend/database.py
+++ backend/database.py
@@ -3,18 +3,11 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import psycopg2
-import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-
-# database_url =  "postgresql://postgres:himani@localhost:5432/search"
+database_url =  "postgresql://postgres:Dheeraj@localhost/shipping"
 
 #creating SQLAlchemy engine
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(database_url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
